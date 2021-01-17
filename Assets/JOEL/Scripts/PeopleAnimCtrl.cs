f4fc9a19ee02684f814c19565ece685fe86dedec
Joel <jsr2k1@gmail.com>
2021-01-17 10:26:08 +0100

    Japan scene with animations

--- Assets/JOEL/Scripts/PeopleAnimCtrl.cs
+++ Assets/JOEL/Scripts/PeopleAnimCtrl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Patrol : MonoBehaviour
+public class PeopleAnimCtrl : MonoBehaviour
 {
     public Transform[] points;
 
@@ -14,36 +14,24 @@ public class Patrol : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
         agent.autoBraking = false;
-
         GotoNextPoint();
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void GotoNextPoint()
     {
-        // Returns if no points have been set up
-        if (points.Length == 0)
+        if(points.Length == 0) {
             return;
-
-        // Set the agent to go to the currently selected destination.
+        }
         agent.destination = points[destPoint].position;
-
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
         destPoint = (destPoint + 1) % points.Length;
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void Update()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
     }
 }
